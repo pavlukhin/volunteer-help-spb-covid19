9ad261acb9cfb4bfe79d4374fe0bdbe74061062f
pavlukhin <vololo100@gmail.com>
2020-04-09 08:53:28 +0300

    Extend Claim class with status fields. "Open" and "In progress" statuses so far.

--- src/main/java/org/ivan/volunteer/claimmap/Claim.java
+++ src/main/java/org/ivan/volunteer/claimmap/Claim.java
@@ -3,15 +3,23 @@ package org.ivan.volunteer.claimmap;
 import java.util.Objects;
 
 public class Claim {
+    public enum Status {
+        OPEN, IN_PROGRESS
+    }
+
     private final String id;
     private final String address;
     private final String details;
+    private final Status status;
 
-    public Claim(String id, String address, String details) {
+    public Claim(String id, String address, String details, Status status) {
         this.id = Objects.requireNonNull(id);
         this.address = Objects.requireNonNull(address);
         this.details = Objects.requireNonNull(details);
+        this.status = Objects.requireNonNull(status);
     }
+
+    // By default getters are needed to include fields to JSON
 
     public String getId() {
         return id;
@@ -23,5 +31,9 @@ public class Claim {
 
     public String getDetails() {
         return details;
+    }
+
+    public Status getStatus() {
+        return status;
     }
 }
--- src/main/java/org/ivan/volunteer/claimmap/ClaimExternalFetcher.java
+++ src/main/java/org/ivan/volunteer/claimmap/ClaimExternalFetcher.java
@@ -20,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class ClaimExternalFetcher {
     public static final String SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1vTBEpyL_pp7MtnmCe7_Z-BG8WfDBg5nTapRQBjHp1z0/export?format=csv&id=1vTBEpyL_pp7MtnmCe7_Z-BG8WfDBg5nTapRQBjHp1z0&gid=0";
+
     private final RestOperations restClient;
 
     public ClaimExternalFetcher() {
@@ -41,17 +42,31 @@ public class ClaimExternalFetcher {
     private static List<Claim> filterClaims(CSVParser parser) {
         ArrayList<Claim> claims = new ArrayList<>();
         for (CSVRecord csvRec : parser) {
-            String status = csvRec.get(1);
-            if (!status.toLowerCase().contains("свободна")) {
+            String statusStr = csvRec.get(1);
+            Claim.Status status = parseStatus(statusStr);
+            if (status == null) {
                 continue;
             }
             String rawId = csvRec.get(0);
             String address = csvRec.get(3);
             String details = csvRec.get(4);
             validateId(rawId)
-                .ifPresent(validId -> claims.add(new Claim(validId, address, details)));
+                .ifPresent(validId -> claims.add(new Claim(validId, address, details, status)));
         }
         return claims;
+    }
+
+    private static Pattern inProgressClaimPattern = Pattern.compile("в +обработке", Pattern.CASE_INSENSITIVE);
+
+    private static Claim.Status parseStatus(String statusStr) {
+        if (statusStr.toLowerCase().contains("свободна")) {
+            return Claim.Status.OPEN;
+        }
+        Matcher matcher = inProgressClaimPattern.matcher(statusStr);
+        if (matcher.find()) {
+            return Claim.Status.IN_PROGRESS;
+        }
+        return null;
     }
 
     private static Pattern claimIdPattern = Pattern.compile("\\d\\d\\d\\d\\d+");
