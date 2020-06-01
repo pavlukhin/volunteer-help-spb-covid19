ee511a1c6c781d997c860aa66b3bbe82a1dcd184
pavlukhin <vololo100@gmail.com>
2020-06-01 13:28:05 +0300

    Remove seemingly unneeded PropertySourcesPlaceholderConfigurer bean from ApplicationStarter.

--- src/main/java/org/ivan/volunteer/claimmap/ApplicationStarter.java
+++ src/main/java/org/ivan/volunteer/claimmap/ApplicationStarter.java
@@ -3,8 +3,6 @@ package org.ivan.volunteer.claimmap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -13,10 +11,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStarter.class, args);
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
     }
 }
